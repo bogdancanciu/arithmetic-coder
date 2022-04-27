5a62df8a5be367eb353353936abc76ba742edafc
bogdancanciu <bogdancanciu21@gmail.com>
2022-04-27 23:52:39 +0300

    updated file_parser.py

--- arithmetic_coding/file_parser.py
+++ arithmetic_coding/file_parser.py
@@ -1,9 +1,16 @@
 class FileParser:
-    def __init__(self,file_name: str) -> None:
-        self.__input_file = file_name #open(file_name,"rb")
-        
-#with open("test.txt","rb") as input_file:
-#    current_byte = input_file.read(1)
-#    while current_byte:
-#        current_byte = input_file.read(1)
-#    my_byte = current_byte[0]
+    #The class constructor is used to initialize the input file reader object.
+    def __init__(self,file_name: str):
+        self.__input_file = open(file_name,"rb")
+    def read_byte(self) -> int:
+        """
+        read_byte method is meant to read a single byte from the input file
+        and to return its decimal ASCII value in case the symbol is found,
+        otherwise it'll close the file and return the 256 value,
+        so we can keep track of the EOF symbol.
+        """
+        try:
+            return self.__input_file.read(1)[0]
+        except IndexError:
+            self.__input_file.close()
+            return 256
